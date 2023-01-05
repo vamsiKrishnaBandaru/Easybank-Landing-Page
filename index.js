fc8311ae9bdbc28f892b93fc1acf090f51c57cb3
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-05 11:48:48 +0530

    added menu bar for mobile view

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function myFunction() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.hamburger')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    var openDropdown = dropdowns[i];
     if (openDropdown.classList.contains('show')) {
       openDropdown.classList.remove('show');
     }
