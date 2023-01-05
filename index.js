bafde4b7fef568794a370204546b4678328f303e
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-05 13:23:08 +0530

    adjusted spacings

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("hideDownPage").classList.toggle("hidePage");
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.hamburger')) {
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
-    }
-  }
-}
+// window.onclick = function (event) {
+//   if (!event.target.matches('.hamburger')) {
+//     if (openDropdown.classList.contains('show')) {
+//       openDropdown.classList.remove('show');
+//     }
+//   }
+// }
