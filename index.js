a54974b3e797f3740f3527d5184c6238a5513ea0
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-05 14:27:35 +0530

    written logic for hidign page-1

--- index.js
+++ index.js
@@ -2,12 +2,3 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
   document.getElementById("hideDownPage").classList.toggle("hidePage");
 }
-
-// Close the dropdown if the user clicks outside of it
-// window.onclick = function (event) {
-//   if (!event.target.matches('.hamburger')) {
-//     if (openDropdown.classList.contains('show')) {
-//       openDropdown.classList.remove('show');
-//     }
-//   }
-// }
